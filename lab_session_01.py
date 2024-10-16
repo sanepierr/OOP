ec604e4d903eaea7e4572b842881c9bddd1e0577
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2024-10-16 23:18:18 +0300

    truck_class

--- lab_session_01.py
+++ lab_session_01.py
@@ -10,6 +10,7 @@ class Vehicle:
         return f"This vehicle is {self.color}"
 
 
+
 class Car(Vehicle):
     def __init__(self, color, winter_tires=False):
         super().__init__(color)  
@@ -18,5 +19,40 @@ class Car(Vehicle):
     def toString(self):
         base_class_func = super().toString()
         return f"{base_class_func}\nHas winter tires: {self.winter_tires}"
+
+
+
+class Truck(Vehicle):
+    def __init__(self, color, has_trailer=False):
+        super().__init__(color)  
+        self.has_trailer = has_trailer
+
+    def toString(self):
+        base_class_func = super().toString()
+        return f"{base_class_func}\nHas trailer: {self.has_trailer}"
+
+
+
+
+class Car(Vehicle):
+    def __init__(self, color, winter_tires=False):
+        super().__init__(color)
+        self.winter_tires = winter_tires
+
+    def toString(self):
+        base_string = super().toString()
+        return f"{base_string}\nHas winter tires: {self.winter_tires}"
+
+class Truck(Vehicle):
+    def __init__(self, color, trailer=False):
+        super().__init__(color)
+        self.trailer = trailer
+
+    def toString(self):
+        base_string = super().toString()
+        return f"{base_string}\nHas trailer: {self.trailer}"
+
+
+
 
 
